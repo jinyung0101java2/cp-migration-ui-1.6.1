e6f566d401d20dc0efb0ef6df5b2c9c67c69f74c
jinyung0101java2 <jinyung0101java2@gmail.com>
2025-04-03 14:19:32 +0900

    Update

--- src/main/java/org/container/platform/web/ui/common/ConstantsUrl.java
+++ src/main/java/org/container/platform/web/ui/common/ConstantsUrl.java
@@ -9,34 +9,17 @@ package org.container.platform.web.ui.common;
  */
 public class ConstantsUrl {
     public static final String URI_CP_CONFIGS_CONFIGMAPS = "/container-platform/configMaps";//
-
-    public static final String URI_CP_GLOBAL_CLUSTERS = "/global/clusters";//
-    public static final String URI_CP_GLOBAL_CLOUD_ACCOUNTS = "/global/cloudAccounts";//
-    public static final String URI_CP_GLOBAL_INSTANCE_CODE_TEMPLATES = "/global/templates";//
-    public static final String URI_CP_GLOBAL_SSH_KEYS = "/global/sshKeys";//
-
-    public static final String URI_CP_LIST = "/list";
     public static final String URI_CP_DETAILS = "/details";
-    public static final String URI_CP_CREATE = "/create";
-    public static final String URI_CP_UPDATE = "/update";
-    public static final String URI_CP_POPUP = "/popup";
-    public static final String URI_CP_LOGS = "/logs";
-
-    public static final String URI_CP_ADD = "/add";
-    public static final String URI_CP_UPGRADE = "/upgrade";
     public static final String URI_CP_GET_USER_LOGIN_DATA = "/container-platform/userLoginData";
     public static final String URI_CP_REFRESH_TOKEN = "/container-platform/refreshToken";
 
     public static final String URI_CP_SESSION_OUT = "/sessionout";
-    public static final String URI_AUTHENTICATION_FAILED = "/error/authenticationFailed";
-    public static final String URI_CP_LOGOUT ="/logout";
     public static final String URl_CP_INACTIVE = "/inactive";
 
     public static final String URI_API_SET_CLUSTER_AUTHORITY="/setAuthority";
 
     //CP-API REQUEST URI
     public static final String URL_API_LOGIN = "/login";
-    public static final String URL_API_SIGNUP = "/signUp";
 
     public static final String URI_API_REFRESH_TOKEN = "/refreshtoken";
 
